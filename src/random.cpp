58cac36fdb94856b25db27ec86326d963df9baac
Pieter Wuille <pieter@wuille.net>
2024-03-10 23:38:31 -0400

    random: make GetRand() support entire range (incl. max)
    The existing code uses GetRand(nMax), with a default value for nMax, where nMax is the
    range of values (not the maximum!) that the output is allowed to take. This will always
    miss the last possible value (e.g. GetRand<uint32_t>() will never return 0xffffffff).
    
    Fix this, by moving the functionality largely in RandomMixin, and also adding a
    separate RandomMixin::rand function, which returns a value in the entire (non-negative)
    range of an integer.

--- src/random.cpp
+++ src/random.cpp
@@ -669,11 +669,6 @@ void GetStrongRandBytes(Span<unsigned char> bytes) noexcept { ProcRand(bytes.dat
 void RandAddPeriodic() noexcept { ProcRand(nullptr, 0, RNGLevel::PERIODIC); }
 void RandAddEvent(const uint32_t event_info) noexcept { GetRNGState().AddEvent(event_info); }
 
-uint64_t GetRandInternal(uint64_t nMax) noexcept
-{
-    return FastRandomContext().randrange(nMax);
-}
-
 uint256 GetRandHash() noexcept
 {
     uint256 hash;
